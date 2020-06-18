addc470475edf33a4c64c0119bdbc6e756d5b42b
ycsjasen <54119411+ycsjasen@users.noreply.github.com>
2020-06-18 02:23:00 -0400

    Updating for SPY instead of IXIC

--- Data preparation.py
+++ Data preparation.py
@@ -2,22 +2,21 @@ import numpy as np
 import pandas as pd
 
 #need to figure out how to substring file name for Ticker
-df = pd.read_csv('../MSFT.csv')
+df = pd.read_csv('MSFT.csv')
 df['Ticker'] = 'MSFT'
 
-IXIC = pd.read_csv('../^IXIC.csv')
-IXIC['Ticker'] = 'IXIC'
+SPY = pd.read_csv('SPY.csv')
+SPY['Ticker'] = 'SPY'
 
 #calculating derived variables
 df['Intraday Range'] = abs(df['Open'] - df['High'])
 df['IntChange'] = df['Close'].diff()
 df['IntPctChange'] = df['Close'].pct_change()*100
 df['200SMA'] = df['Close'].rolling(window = 200).mean()
-IXIC['IntPctChange'] = IXIC['Close'].pct_change()*100
-df['IXIC_PctDiff'] =  df['IntPctChange'] - IXIC['IntPctChange']
+SPY['IntPctChange'] = SPY['Close'].pct_change()*100
+df['SPY_PctDiff'] =  df['IntPctChange'] - SPY['IntPctChange']
 
 #removing missing values
 df = df.dropna()
 
-print(df)
 df.to_csv('MSFT_updated.csv', sep=',', index = False)
